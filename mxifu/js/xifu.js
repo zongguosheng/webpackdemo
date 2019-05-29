2cd042cc592d8d83f012b5d60196227b1959394a
zongbojue <zonzxcv@gmail.com>
2019-05-29 14:43:31 +0800

    mxifu

---
+++ mxifu/js/xifu.js
@@ -0,0 +1,45 @@
+// Init App
+var myApp = new Framework7({
+    modalTitle: 'Framework7',
+    
+    material: true,
+});
+
+
+var $$ = Dom7;
+
+var mySwiperSlow = myApp.swiper('.swiper-slow', {
+  pagination:'.swiper-slow .swiper-pagination',
+  speed: 600
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
